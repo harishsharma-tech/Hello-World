31fe8670d981a7e3c262f41f687a944821f23484
harishsharma-tech <hs0964@gmail.com>
2021-01-26 19:08:02 +0530

    Added Index.html and script.js

---
+++ script.js
@@ -0,0 +1,59 @@
+// data types in javascript
+// primitive data types
+//STRINGS
+var str = "Harish Sharma";
+var str2 = 'Harish Sharma';
+
+// console.log(str);
+// console.log(str2);
+
+// NUMBER DATA TYPES
+
+var x;
+var y = 4.3;
+
+// console.log(x);
+// console.log(typeof y, y);
+
+// BIGINT
+
+var bigInt = 1713948173290578172309710297369017293075172371273897512903750917283979102735;
+//console.log(typeof bigInt, bigInt);
+
+// BOOLEAN
+var a=true;
+var b=3;
+var c=4;
+// console.log(typeof a, a==b);
+// console.log(typeof b, c==c);
+
+// UNDEFINED
+var val = 23;
+var val = undefined;
+
+
+// NULL
+
+var val = null;
+
+// SYMBOL
+
+var val = Symbol('Symbol');
+//NON PRIMITIVE DATA TYPES
+// OBJECTS IS USED TO STORE KEY-VALUE PAIRS
+
+var val = {
+    x:43,
+    y:"hello World",
+    z:function(){
+        return "this.x;"
+    }
+}
+
+//ARRAYS COLLECTION OF DATA IN ORDERED LIST
+
+var val = [2,true,"hello World",undefined,null]
+
+console.log("typeof val is: " + typeof val, ", " + "value of val: " + val)
+
+//b8ec2a87cdf1eb2ad2be3f984b68edd1b2668d99 gitpat
